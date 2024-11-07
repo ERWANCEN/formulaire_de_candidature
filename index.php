82df145e5556d681a386bba834a239acedc52fc4
ERWANCEN <erwance@hotmail.fr>
2024-11-07 17:41:28 +0100

    je ne sais plus quoi mettre

--- index.php
+++ index.php
@@ -4,7 +4,6 @@ include 'date.php';
 include 'email.php';
 include 'message.php';
 include 'prenom_nom.php';
-// include 'messages_erreur.php';
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
       include 'envoi_email.php';
   }
@@ -32,6 +31,38 @@ $services = [
       ]
   ];
 
+  $prenomError = $nomError = $emailError = $disponibiliteError = $messageError = "";
+
+  if ($_SERVER["REQUEST_METHOD"] == "POST") {
+      $email = $_POST['email'] ?? '';
+      $prenom = $_POST['prenom'] ?? '';
+      $nom = $_POST['nom'] ?? '';
+      $disponibilite = $_POST['disponibilite'] ?? '';
+      $message = $_POST['message'] ?? '';
+      $prenom = ucfirst(strtolower($prenom));
+      $nom = strtoupper($nom);	
+  
+      if (!verificationPrenomNom($prenom)) {
+          $prenomError = "Le prénom doit commencer par une majuscule suivie de lettres minuscules uniquement et pas de chiffre";
+      }
+  
+      if (!verificationPrenomNom($nom)) {
+          $nomError = "Le nom doit commencer par une majuscule suivie de lettres minuscules uniquement et pas de chiffre";
+      }
+   
+      if (verificationSpam($email)) {
+          $emailError = "L'email est considéré comme spam et a été bloqué";
+      }
+  
+      if (!verificationDate($disponibilite)) {
+          $disponibiliteError = "La date de disponibilité ne peut pas etre inférieur à la date actuelle, sa doit etre dans le futur";
+      }
+  
+      if (!verificationMessage($message)) {
+        $messageError = "Le message doit contenir entre 20 et 200 caractères";
+    }
+  }
+
 ?>
 
 <!DOCTYPE html>
@@ -48,29 +79,34 @@ $services = [
 
       <div class="container my-5">
             <h2 class="mb-4">Formulaire de Candidature</h2>
+
             <form action="" method="post">
 
                   <!-- Prénom et Nom -->
                   <div class="mb-3">
+                        <?php if ($prenomError) { ?>
+                              <div class="alert alert-danger" role="alert"><?php echo $prenomError; ?></div>
+                        <?php } ?>
                         <label for="prenom" class="form-label">Prénom</label>
                         <input type="text" class="form-control" id="prenom" name="prenom" required>
-                        <p><?php if(isset($errors['prenom'])) {
-                              echo $errors['prenom'];
-                        } ?></p>
                   </div>
 
                   <div class="mb-3">
+                        <?php if ($nomError) { ?>
+                              <div class="alert alert-danger" role="alert"><?php echo $nomError; ?></div>
+                        <?php } ?>
                         <label for="nom" class="form-label">Nom</label>
                         <input type="text" class="form-control" id="nom" name="nom" required>
-                        <p><?php if(isset($errors['nom'])) {
-                              echo $errors['nom'];
-                        } ?></p>
                   </div>
 
                   <!-- Email -->
+                  <!-- On inclut le fichier verification.php pour s'assurer que l'email est valide et est nom un email de spam -->
                   <div class="mb-3">
+                        <?php if ($emailError) { ?>
+                              <div class="alert alert-danger" role="alert"><?php echo $emailError; ?></div>
+                        <?php } ?>
                         <label for="email" class="form-label">Email</label>
-                        <input type="email" class="form-control" id="email" name="email" required>
+                        <input type="email" class="form-control" id="email" name="email" required/>
                   </div>
 
                   <!-- Téléphone -->
@@ -82,7 +118,7 @@ $services = [
                   <!-- Préférence de réception de CV -->
                   <div class="mb-3">
                         <label for="service" class="form-label">Service de transmission du CV</label>
-                        <select class="form-select" id="service" name="service" required>
+                        <select class="form-select" id="service" required>
                               <option value="communication">Service communication</option>
                               <option value="rh">Service ressources humaines</option>
                               <option value="logistique">Service logistique</option>
@@ -98,8 +134,11 @@ $services = [
 
                   <!-- Message de motivation -->
                   <div class="mb-3">
+                        <?php if ($messageError) { ?>
+                              <div class="alert alert-danger" role="alert"><?php echo $messageError; ?></div>
+                        <?php } ?>
                         <label for="message" class="form-label">Message de motivation</label>
-                        <textarea class="form-control" id="message" name="message" rows="5"
+                        <textarea class="form-control" id="message" rows="5"
                               placeholder="Parlez-nous de vous, de vos motivations, et de ce qui vous attire dans ce poste."
                               required></textarea>
                   </div>
@@ -122,12 +161,15 @@ $services = [
 
                   <!-- Disponibilité -->
                   <div class="mb-3">
+                        <?php if ($disponibiliteError) { ?>
+                              <div class="alert alert-danger" role="alert"><?php echo $disponibiliteError; ?></div>
+                        <?php } ?>
                         <label for="disponibilite" class="form-label">Disponibilité</label>
                         <input type="date" class="form-control" id="disponibilite" name="disponibilite" required>
                   </div>
 
                   <!-- Bouton de soumission -->
-                  <button type="submit" class="btn btn-primary" method="post">Envoyer la Candidature</button>
+                  <button type="submit" class="btn btn-primary">Envoyer la Candidature</button>
             </form>
       </div>
 
